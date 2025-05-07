a03b2e777bd15a61df60cbc9ef4225e77caecfe4
hannah <cabaricosalazarhannah@gmail.com>
2025-05-06 19:15:33 -0500

    cambios en el html y en los eventos

--- js/dashboardService.js
+++ js/dashboardService.js
@@ -1,5 +1,17 @@
 function users(){
+    const REQRES_ENDPOINT ='https://reqres.in/api/login'
+    fetch(REQRES_ENDPOINT,{
+
+        method:'POST',
+        headers: {
+            'Content-type':'application/json',
+            'x-api-key':'reqres-free-v1'
+        },
+        body: JSON.stringify({email,password})
+    })
+
 document.getElementById('info').innerHTML='<h1>lista de usuarios </h1>'
+
 
 }
 
